df77d2522103219648f88a41d39085db415d4434
Scraxtastic <nick.u19990@gmail.com>
2022-07-16 02:55:44 +0200

    Added Code Comments

--- Assets/Scripts/ImitateObject.cs
+++ Assets/Scripts/ImitateObject.cs
@@ -6,15 +6,10 @@ public class ImitateObject : MonoBehaviour
 {
 
     [SerializeField] private GameObject target;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
+        //Follows the target on every frame update
         this.transform.position = target.transform.position;
         this.transform.rotation = target.transform.rotation;
     }
