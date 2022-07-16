101de664242cef977c5360b353d40deaa29a48d6
Scraxtastic <nick.u19990@gmail.com>
2022-07-16 04:47:33 +0200

    Added Code Comments

--- Assets/Scripts/DiceHintVisualizer.cs
+++ Assets/Scripts/DiceHintVisualizer.cs
@@ -6,6 +6,7 @@ public class DiceHintVisualizer : MonoBehaviour
 {
     private GameObject currentGameObject;
     [SerializeField] private Camera diceCam;
+    //How many degrees to rotate per second (approx)
     private float rotationSpeed = 60f;
 
     private void Update()
@@ -24,15 +25,19 @@ public class DiceHintVisualizer : MonoBehaviour
 
     public void SetDice(GameObject dice)
     {
+        //Destroys the old dice, if existent
         if (currentGameObject != null)
         {
             Destroy(currentGameObject);
         }
+        //Creates a copy of the dice
         currentGameObject = Instantiate(dice);
         currentGameObject.transform.parent = this.transform;
         currentGameObject.transform.localPosition = new Vector3();
         Rigidbody rb = currentGameObject.GetComponent<Rigidbody>();
+        //Disables gravity
         rb.isKinematic = true;
+        //Sets the layer to UI, so that the DiceCam can see it
         currentGameObject.layer = LayerMask.NameToLayer("UI");
     }
 }
