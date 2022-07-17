2a412f1d2d77dec47ac5c27bd2801f993905b843
developer <developer@local.dev>
2022-07-17 08:21:44 +0200

    Added Spider Walk SFX

--- Assets/Scripts/Input/PlayerController.cs
+++ Assets/Scripts/Input/PlayerController.cs
@@ -14,6 +14,8 @@ public class PlayerController : MonoBehaviour
     private GameControls inputActions;
     private InputAction turn;
     public event Action FallOutOfBounce;
+    private AudioSource audioSource;
+    private Vector3 lastPosition;
 
     private void Awake()
     {
@@ -35,6 +37,15 @@ public class PlayerController : MonoBehaviour
     //    Debug.Log("Performed" + obj.ReadValue<Vector3>());
     //}
 
+    public void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if(audioSource == null)
+        {
+            Debug.LogWarning("Player has no AudioSource");
+        }
+    }
+
     private void OnDisable()
     {
         turn.Disable();
@@ -43,6 +54,8 @@ public class PlayerController : MonoBehaviour
 
     public void Update()
     {
+        lastPosition = transform.position;
+
         //Player rotation
         Vector3 eulerRotation = transform.rotation.eulerAngles;
         transform.rotation = Quaternion.Euler(eulerRotation.x, eulerRotation.y + turn.ReadValue<Vector3>().x * rotationSpeed * Time.deltaTime, 0);
@@ -51,6 +64,25 @@ public class PlayerController : MonoBehaviour
         transform.position += movement * speedMultiplier * Time.deltaTime;
         if (transform.position.y < dieY)
             OnFallOutOfBounce();
+
+        ToggleStepSound(IsMoving());
+    }
+
+    private bool IsMoving()
+    {
+        return lastPosition != transform.position;
+    }
+
+    private void ToggleStepSound(bool enable)
+    {
+        if(!audioSource.isPlaying && enable)
+        {
+            audioSource.Play();
+        }
+        else if(!enable)
+        {
+            audioSource.Stop();
+        }
     }
 
     protected void OnFallOutOfBounce()
