6cd7570ca9060d2750322e6291996152c9f3a0d6
Capt.Pie <50845872+JavaPieChan@users.noreply.github.com>
2022-07-17 15:07:17 +0200

    Add next level function (#20)
    Co-authored-by: developer <developer@local.dev>

--- Assets/Scripts/BackToMenu.cs
+++ Assets/Scripts/BackToMenu.cs
@@ -2,10 +2,42 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class BackToMenu : MonoBehaviour
-{ 
+{
+    public int currentLevelID = 1;
     public void BackToMenuClick()
     {
         Time.timeScale = 1.0f;
         SceneManager.LoadScene(0); // Load Menu
+    }
+
+    public void NextLevel()
+    {
+        Time.timeScale = 1.0f;
+
+        int nextLevelID = 0;
+
+        switch (currentLevelID)
+        {
+            case 1:
+                nextLevelID = 2;
+                break;
+            case 2:
+                nextLevelID = 3;
+                break;
+            case 3:
+                nextLevelID = 4;
+                break;
+            case 4:
+                nextLevelID = 5;
+                break;
+            case 5:
+                nextLevelID = 6;
+                break;
+            case 6:
+                nextLevelID = 0;
+                break;
+        }
+
+        SceneManager.LoadScene(nextLevelID);
     }
 }
